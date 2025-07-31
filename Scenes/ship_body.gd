ef4c855e10203913eb3a711cc112595f38f8f641
zkdavis <zachkdavis00@gmail.com>
2025-07-30 19:42:52 -1000

    init_ship

---
+++ Scenes/ship_body.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var a_scale = 100
+@export var thrust_scale = 10
+
+
+var gravity = Vector2(0,1)*a_scale
+var thrust = Vector2(0,0)*thrust_scale
+
+#func get_input():
+	#pass
+
+
+#func _physics_process(delta: float) -> void:
+	#self.velocity = delta*gravity
+	#self.position = delta*self.velocity
+func _input(event: InputEvent) -> void:
+	var up = Input.is_action_pressed('ui_up')
+	var down = Input.is_action_pressed('ui_down')
+	if up:
+		thrust += Vector2(0,-1)*thrust_scale
+	if down:
+		thrust += Vector2(0,1)*thrust_scale
+	
+func _physics_process(delta: float) -> void:
+	self.velocity += delta*(gravity+thrust)
+	self.position += delta*self.velocity
+	
