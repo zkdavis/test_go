8f5f16192089510e0b490eca2422ab962191c516
zkdavis <zachkdavis00@gmail.com>
2025-07-31 23:11:10 -1000

    playground ready

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,6 +1,46 @@
 extends Node2D
 
 
-#func _ready() -> void:
-	#$boundary.get_node("RigidBody2D/ColorRect").scale = Vector2(10,2)
-	#$boundary.get_node("RigidBody2D/CollisionShape2D").scale = Vector2(10,2)
+var ob_scene : PackedScene = load("res://Scenes/orbiting_body.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Ship_Scene/CharacterBody2D.position = Vector2(300,400)
+	$Ship_Scene/CharacterBody2D.velocity = Vector2(0,-110)
+	
+	$ThrustGauge/Label2.text = str($Ship_Scene.thrust_int)
+	
+	var p1 = ob_scene.instantiate() as Orbiting_Body
+	$Planets.add_child(p1)
+	p1.setup(500.0, 0.5, Vector2(600, 400), Vector2(0, 0))
+	#var p2 = ob_scene.instantiate() as Orbiting_Body
+	#$Planets.add_child(p2)
+	#p2.setup(100.0, 1, Vector2(800, 400), Vector2(0, 0))
+	#var p3 = ob_scene.instantiate() as Orbiting_Body
+	#$Planets.add_child(p3)
+	#p3.setup(10.0, 0.5, Vector2(600, 400), Vector2(0, 0))
+
+
+func calculate_gravitational_force(target : Orbiting_Body, source : Orbiting_Body) -> Vector2:
+	var G = 10000 ## GConstant
+	var direction = target.get_pos() - source.get_pos()
+	var distance = direction.length()
+	if distance == 0:
+		return Vector2.ZERO ## Safeguard
+	var force_magnitude = G * target.get_mass() * source.get_mass() / (distance*distance)
+	return -direction.normalized() * force_magnitude
+
+func _physics_process(_delta: float) -> void:
+	var parent_node = get_node("Planets")
+	var num_children = parent_node.get_child_count()
+	# Clear the forces
+	for child in parent_node.get_children():
+		child.clear_force()
+	# Apply forces
+	for i in range(num_children):
+		for j in range(i):
+			var target = parent_node.get_child(i) as Orbiting_Body
+			var source = parent_node.get_child(j) as Orbiting_Body
+			var force = calculate_gravitational_force(target, source)
+			target.apply_force(force)
+			source.apply_force(-force)
--- Scenes/ship_scene.gd
+++ Scenes/ship_scene.gd
@@ -1,52 +1,70 @@
 extends Node2D
 
 @export var a_scale = 0
-@export var thrust_scale = 10
+@export var thrust_scale = 50
 @export var thrust_scale_rotate = 0.01
+@export var mass = 1
 
 var gravity = Vector2(0,1)*a_scale
 var main_thrust = 0*thrust_scale
 var thrust_rotate = 0.0*thrust_scale_rotate
-var thrust = 0
+var thrust_int: int = 0
+var max_thrust_int: int = 20
 var orientation = Vector2(0,-1)
 var angular_vel = 0.0
+var bods = []
 
-
-func _ready() -> void:
-	self.position = get_window().size/2
-	self.position.y +=100
 
 
 func get_input():
-	var up = Input.is_action_pressed('ui_up')
-	var down = Input.is_action_pressed('ui_down')
+	var up = Input.is_action_just_pressed('ui_up')
+	var down = Input.is_action_just_pressed('ui_down')
 	var left = Input.is_action_pressed('ui_left')
 	var right = Input.is_action_pressed('ui_right')
 
 	if up:
-		main_thrust += 1*thrust_scale
+		thrust_int +=1
+		thrust_int = clampi(thrust_int,-max_thrust_int,max_thrust_int)
+		self.get_parent().get_node("ThrustGauge/Label2").text = str(thrust_int)
 	if down:
-		main_thrust += -1*thrust_scale
+		thrust_int -=1
+		thrust_int = clampi(thrust_int,-max_thrust_int,max_thrust_int)
+		self.get_parent().get_node("ThrustGauge/Label2").text = str(thrust_int)
+	main_thrust = thrust_int*thrust_scale
 	if right:
-		#$CharacterBody2D.rotation += 0.01
-		thrust_rotate += clamp(1.0*thrust_scale_rotate,-3*thrust_scale_rotate,3*thrust_scale_rotate)
+		$CharacterBody2D.rotation += 0.1
+		#thrust_rotate += clamp(1.0*thrust_scale_rotate,-3*thrust_scale_rotate,3*thrust_scale_rotate)
 	if left:
-		#$CharacterBody2D.rotation -= 0.01
-		thrust_rotate += clamp(-1.0*thrust_scale_rotate,-3*thrust_scale_rotate,3*thrust_scale_rotate)
+		$CharacterBody2D.rotation -= 0.1
+		#thrust_rotate += clamp(-1.0*thrust_scale_rotate,-3*thrust_scale_rotate,3*thrust_scale_rotate)
+		
+func calculate_gravitational_force(bodies) -> Vector2:
+	var G = 10000 ## GConstant
+	var force = Vector2(0,0)
+	for b in bodies:
+		var direction = b.get_pos() - $CharacterBody2D.global_position
+		var distance = direction.length()
+		if distance <= 1e-1:
+			return Vector2.ZERO ## Safeguard
+		var force_magnitude = G * b.get_mass() * self.mass / (distance*distance)
+		force += direction.normalized() * force_magnitude
+	return force/self.mass
+	
+func get_bods():
+	var bs = get_parent().get_node("Planets").get_children()
+	for b in bs:
+		bods.append(b)
+	return bods
+		
 		
 func _physics_process(delta: float) -> void:
+	get_bods()
 	get_input()
-	thrust = main_thrust
-	##apply rotation to thrust
-	#var w_size = get_window().size/2
-	##var center_r = self.position - Vector2(w_size[0],w_size[1])
-	##if center_r.length() > 2:
-		##gravity = -a_scale*center_r/(center_r.length()**(5/2))
-	##else:
-		##gravity = Vector2(0,0)
-	$CharacterBody2D.velocity += delta*(gravity + (thrust*Vector2(sin($CharacterBody2D.rotation),-cos($CharacterBody2D.rotation))))
-	#
-	self.angular_vel += thrust_rotate*delta
+	
+	gravity = calculate_gravitational_force(bods)
+	bods = []
+	$CharacterBody2D.velocity += delta*(gravity + (main_thrust*Vector2(sin($CharacterBody2D.rotation),-cos($CharacterBody2D.rotation))))
+	#self.angular_vel += thrust_rotate*delta
 	$CharacterBody2D.rotate(self.angular_vel*delta)
 	##if(abs(self.angular_vel) < 2*delta*thrust_scale_rotate and abs(self.thrust_rotate)<3*thrust_scale_rotate ):
 		##self.angular_vel*=exp(-3*delta)
