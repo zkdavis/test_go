fdbcca3045d301d7a73df7426088b57c2d33b63a
Michael Vennettilli <mikepvenn@gmail.com>
2025-08-01 22:05:36 -0700

    added speed label

---
+++ Scenes/speed_label.gd
@@ -0,0 +1,13 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func set_fuel_label() -> void:
+	self.text = "Speed: %s" % get_parent().get_node("Ship_Scene/CharacterBody2D").velocity.length()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	set_fuel_label()
