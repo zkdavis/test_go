7ac7bc61a26bb150aa07578caad8af2873362347
zkdavis <zachkdavis00@gmail.com>
2025-08-01 22:24:32 -1000

    fixed stuff

--- Scenes/speed_label.gd
+++ Scenes/speed_label.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func set_fuel_label() -> void:
-	self.text = "Speed: %s" % get_parent().get_node("Ship_Scene/CharacterBody2D").velocity.length()
+	self.text = "Speed: %s" % $"../../CharacterBody2D".velocity.length()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
